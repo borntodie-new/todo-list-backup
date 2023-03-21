45eb6bc2f69e8ab4398914f4d96153e6ac7d89c1
jason <2631937454@qq.com>
2023-03-21 16:02:30 +0800

    :white_check_mark: 完成查询多个用户记录的测试用例

---
+++ service/user/mget_user_test.go
@@ -0,0 +1,43 @@
+package user
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"testing"
+	"time"
+)
+
+var db2 *gorm.DB
+
+func init() {
+	var err error
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second,   // 慢 SQL 阈值
+			LogLevel:      logger.Silent, // Log level
+			Colorful:      false,         // 禁用彩色打印
+		},
+	)
+	dsn := "root:123456@tcp(192.168.226.130:3306)/todo?charset=utf8mb4&parseTime=True&loc=Local"
+	db2, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func TestMGetUser(t *testing.T) {
+	userIds := []int64{1, 2}
+	data, err := MGetUser(userIds, context.Background(), db2)
+	assert.Nil(t, err)
+	for _, item := range data {
+		t.Log(item.Username, "     ", len(item.Todos))
+	}
+}
