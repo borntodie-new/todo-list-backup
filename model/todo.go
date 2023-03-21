eb38cc3f48b5f67c45005d661806bbcdca5454d7
jason <2631937454@qq.com>
2023-03-21 11:22:01 +0800

    :bug: 修复todo模型表的字段信息&&新增查询单条记录方法&&修改更新逻辑

--- model/todo.go
+++ model/todo.go
@@ -12,11 +12,11 @@ import (
 type Todo struct {
 	ID        int64          `json:"id" gorm:"column:id;primarykey"`
 	UserId    int64          `json:"user_id" gorm:"column:user_id"`
-	Context   string         `json:"username" gorm:"column:context"`
+	Content   string         `json:"content" gorm:"column:content"`
 	Completed bool           `json:"completed" gorm:"column:completed;default:false"`
 	CreatedAt time.Time      `json:"created_at" gorm:"column:created_at"`
 	UpdatedAt time.Time      `json:"updated_at" gorm:"column:updated_at"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"column:updated_at;index"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"column:deleted_at;index"`
 }
 
 func (*Todo) TableName() string {
@@ -40,17 +40,22 @@ func NewTodoDao(ctx context.Context, db *gorm.DB) *TodoDao {
 	return todoDao
 }
 
-func (d *TodoDao) CreateInstance(todo *TodoDao) error {
+func (d *TodoDao) CreateInstance(todo *Todo) error {
 	return d.db.WithContext(d.ctx).Create(&todo).Error
 }
-func (d *TodoDao) DeleteInstance(id int64) error {
-	return d.db.WithContext(d.ctx).Where("id = ?", id).Update("completed", true).Error
+func (d *TodoDao) DeleteInstance(userId, id int64) error {
+	return d.db.WithContext(d.ctx).Model(&Todo{}).Where("id = ? and user_id = ?", id, userId).Update("completed", true).Error
 }
 func (d *TodoDao) RetrieveInstances(userId int64, offset, limit int) ([]*Todo, error) {
 	ts := make([]*Todo, 0)
-	err := d.db.WithContext(d.ctx).Where("user_id = ?", userId).Limit(limit).Offset(offset).Order("age create_at").Find(&ts).Error
+	err := d.db.WithContext(d.ctx).Where("user_id = ?", userId).Limit(limit).Offset(offset).Order("created_at DESC").Find(&ts).Error
 	return ts, err
 }
-func (d *TodoDao) UpdateInstance(id int64, content string) error {
-	return d.db.WithContext(d.ctx).Where("id = ?", id).Update("context", content).Error
+func (d *TodoDao) UpdateInstance(userId, id int64, content string) error {
+	return d.db.WithContext(d.ctx).Model(&Todo{}).Where("id = ? and user_id = ?", id, userId).Update("context", content).Error
+}
+func (d *TodoDao) RetrieveInstance(userId, id int64) (*Todo, error) {
+	t := &Todo{}
+	err := d.db.WithContext(d.ctx).Where("user_id = ? and id = ?", userId, id).Find(t).Error
+	return t, err
 }
