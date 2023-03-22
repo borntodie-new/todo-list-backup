826c12a876ad7baa91ea1fe94a4dbdfdf155f39c
jason <2631937454@qq.com>
2023-03-22 22:29:18 +0800

    :pencil2: 修改登录&&注册逻辑-验证码部分

--- handler/user/login.go
+++ handler/user/login.go
@@ -1,8 +1,12 @@
 package user
 
 import (
+	"fmt"
+	"github.com/borntodie-new/todo-list-backup/constant"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 	"net/http"
+	"strings"
 
 	resp "github.com/borntodie-new/todo-list-backup/handler"
 	service "github.com/borntodie-new/todo-list-backup/service/user"
@@ -17,11 +21,30 @@ type LoginRequest struct {
 func (h *Handler) Login(ctx *gin.Context) {
 	req := new(LoginRequest)
 	if err := ctx.ShouldBindJSON(&req); err != nil {
-		ctx.JSON(http.StatusOK, resp.RespFailed(err))
+		ctx.JSON(http.StatusOK, resp.RespFailed(constant.ParamErr))
+		return
 	}
-	user, err := service.RetrieveUser(req.Username, req.Password, req.Code, ctx, h.db, h.rd)
+	// handler code here
+	key := fmt.Sprintf(constant.CodePrefix, req.Username)
+	cacheCode, err := h.rd.Get(ctx, key).Result()
+	if err == redis.Nil {
+		ctx.JSON(http.StatusOK, resp.RespFailed(constant.CodeExpiresErr))
+		return
+	}
+	if err != nil {
+		ctx.JSON(http.StatusOK, resp.RespFailed(constant.CodeIncorrectErr))
+		return
+	}
+	if strings.ToLower(cacheCode) != strings.ToLower(req.Code) {
+		ctx.JSON(http.StatusOK, resp.RespFailed(constant.CodeIncorrectErr))
+		return
+	}
+	user, err := service.RetrieveUser(req.Username, req.Password, ctx, h.db)
 	if err != nil {
 		ctx.JSON(http.StatusOK, resp.RespFailed(err))
+		return
 	}
+	// TODO signed token here
+
 	ctx.JSON(http.StatusOK, resp.RespSuccessWithData(user))
 }
--- handler/user/register.go
+++ handler/user/register.go
@@ -1,10 +1,14 @@
 package user
 
 import (
+	"fmt"
+	"github.com/borntodie-new/todo-list-backup/constant"
 	resp "github.com/borntodie-new/todo-list-backup/handler"
 	service "github.com/borntodie-new/todo-list-backup/service/user"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 	"net/http"
+	"strings"
 )
 
 type RegisterRequest struct {
@@ -18,10 +22,25 @@ type RegisterRequest struct {
 func (h *Handler) Register(ctx *gin.Context) {
 	req := new(RegisterRequest)
 	if err := ctx.ShouldBindJSON(req); err != nil {
-		ctx.JSON(http.StatusOK, resp.RespFailed(err))
+		ctx.JSON(http.StatusOK, resp.RespFailed(constant.ParamErr))
 		return
 	}
-	if err := service.CreateUser(req.Username, req.Password, req.Email, req.Code, req.Avatar, ctx, h.db, h.rd); err != nil {
+	// handler code here
+	key := fmt.Sprintf(constant.CodePrefix, req.Username)
+	cacheCode, err := h.rd.Get(ctx, key).Result()
+	if err == redis.Nil {
+		ctx.JSON(http.StatusOK, resp.RespFailed(constant.CodeExpiresErr))
+		return
+	}
+	if err != nil {
+		ctx.JSON(http.StatusOK, resp.RespFailed(constant.CodeIncorrectErr))
+		return
+	}
+	if strings.ToLower(cacheCode) != strings.ToLower(req.Code) {
+		ctx.JSON(http.StatusOK, resp.RespFailed(constant.CodeIncorrectErr))
+		return
+	}
+	if err = service.CreateUser(req.Username, req.Password, req.Email, req.Avatar, ctx, h.db); err != nil {
 		ctx.JSON(http.StatusOK, resp.RespFailed(err))
 		return
 	}
