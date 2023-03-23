49ce8b6359a901f907b29117545d9ba71aabce8a
jason <2631937454@qq.com>
2023-03-23 13:52:01 +0800

    :hammer: 完成token的生成、验证、刷新方法

---
+++ utils/jwt.go
@@ -0,0 +1,59 @@
+package utils
+
+import (
+	"github.com/borntodie-new/todo-list-backup/constant"
+	"github.com/golang-jwt/jwt/v4"
+	"time"
+)
+
+type Claims struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+type CustomJWT struct {
+	SigningKey []byte
+}
+
+func NewCustomJWT(privateKey []byte) *CustomJWT {
+	return &CustomJWT{
+		SigningKey: privateKey,
+	}
+}
+
+// GenerateToken generate token by claims
+func (j *CustomJWT) GenerateToken(claims *Claims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(j.SigningKey)
+}
+
+// ParseToken parse token by token string
+func (j *CustomJWT) ParseToken(tokenString string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return j.SigningKey, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, constant.TokenInvalidErr
+}
+
+// RefreshToken refresh token by token string
+func (j *CustomJWT) RefreshToken(tokenString string) (string, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return j.SigningKey, nil
+	})
+	if err != nil {
+		return "", err
+	}
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		claims.ExpiresAt = jwt.NewNumericDate(time.Now().Add(constant.TokenExpires * time.Hour)) // refresh token expire time is 24 hour
+		jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		return token.SignedString(j.SigningKey)
+	}
+	return "", constant.TokenInvalidErr
+}
