553c9f52713c553a98fe329e658b24ffa4cfeb45
jason <2631937454@qq.com>
2023-03-25 13:09:16 +0800

    :sparkles: 完成添加todo记录接口

---
+++ handler/todo/add_todo.go
@@ -0,0 +1,28 @@
+package todo
+
+import (
+	"github.com/borntodie-new/todo-list-backup/constant"
+	resp "github.com/borntodie-new/todo-list-backup/handler"
+	service "github.com/borntodie-new/todo-list-backup/service/todo"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type AddTodoRequest struct {
+	UserId  int64  `json:"user_id" binding:"required"`
+	Content string `json:"content" binding:"required"`
+}
+
+func (h *HandlerService) AddTodo(ctx *gin.Context) {
+	req := new(AddTodoRequest)
+	if err := ctx.ShouldBindJSON(req); err != nil {
+		ctx.JSON(http.StatusOK, resp.RespFailed(constant.ParamErr))
+		return
+	}
+	err := service.CreateTodo(req.UserId, req.Content, ctx, h.db)
+	if err != nil {
+		ctx.JSON(http.StatusOK, resp.RespFailed(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, resp.RespSuccess())
+}
