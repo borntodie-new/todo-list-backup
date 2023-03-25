7a59fca373ef809745b90cadf4a925529305691b
jason <2631937454@qq.com>
2023-03-25 13:20:54 +0800

    :sparkles: 完成删除todo记录-修改completed字段

---
+++ handler/todo/del_todo.go
@@ -0,0 +1,24 @@
+package todo
+
+import (
+	"github.com/borntodie-new/todo-list-backup/constant"
+	resp "github.com/borntodie-new/todo-list-backup/handler"
+	service "github.com/borntodie-new/todo-list-backup/service/todo"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+func (h *HandlerService) DelTodo(ctx *gin.Context) {
+	// get userId from ctx
+	userId := ctx.MustGet(constant.IDOfContextKey).(int64)
+	// get todoId from url's params
+	todoIdStr := ctx.Param("id")
+	todoId, _ := strconv.ParseInt(todoIdStr, 10, 64)
+	err := service.DeleteTodo(userId, todoId, ctx, h.db)
+	if err != nil{
+		ctx.JSON(http.StatusOK, resp.RespFailed(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, resp.RespSuccess())
+}
